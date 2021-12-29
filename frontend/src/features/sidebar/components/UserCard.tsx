61b11d366630e9f7097931b2c2d370df8105b4c1
Levan Kvirkvelia <lekvirkvelia@gmail.com>
2021-12-29 17:58:00 +0300

    Add connect button

--- frontend/src/features/sidebar/components/UserCard.tsx
+++ frontend/src/features/sidebar/components/UserCard.tsx
@@ -1,13 +1,6 @@
-import { Avatar, Badge, Flex } from '@chakra-ui/react';
+import { Avatar, Badge, Button, Flex } from '@chakra-ui/react';
 import { useWeb3 } from '../../web3/components/Web3Provider';
-//			<button
-// 				onClick={async () => {
-// 					if (isWalletSelected) disconnectWallet();
-// 					else await selectWallet();
-// 				}}
-// 			>
-// 				{isWalletSelected ? 'Disconnect' : 'Connect'}
-// 			</button>
+
 export function UserCard() {
 	const { disconnectWallet, selectWallet, isWalletSelected, address, wallet } = useWeb3();
 	return (
@@ -15,18 +8,29 @@ export function UserCard() {
 			<Avatar src={wallet?.icons.iconSrc} mr="1" />
 			<Flex minW="0" direction="column">
 				ds
-				<Badge
-					overflow="hidden"
-					textOverflow="ellipsis"
-					wordBreak="break-word"
-					overflowWrap="break-word"
-					rounded="md"
-					px="2"
-					fontSize="0.8em"
-					colorScheme="gray"
-				>
-					{address}
-				</Badge>
+				{isWalletSelected ? (
+					<Badge
+						overflow="hidden"
+						textOverflow="ellipsis"
+						wordBreak="break-word"
+						overflowWrap="break-word"
+						rounded="md"
+						px="2"
+						fontSize="0.8em"
+						colorScheme="gray"
+					>
+						{address}
+					</Badge>
+				) : (
+					<Button
+						size="sm"
+						onClick={async () => {
+							await selectWallet?.();
+						}}
+					>
+						Connect
+					</Button>
+				)}
 			</Flex>
 		</Flex>
 	);
