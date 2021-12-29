9215c302fb8a8ef30ba5fb0a5dfa0302bd4b6f62
Levan Kvirkvelia <lekvirkvelia@gmail.com>
2021-12-29 17:54:49 +0300

    manifest

--- frontend/configs/webpack/prod.js
+++ frontend/configs/webpack/prod.js
@@ -1,17 +1,27 @@
 // production config
-const { merge } = require("webpack-merge");
-const { resolve } = require("path");
+const { merge } = require('webpack-merge');
+const { resolve } = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const commonConfig = require("./common");
+const commonConfig = require('./common');
 
 module.exports = merge(commonConfig, {
-  mode: "production",
-  entry: "./index.tsx",
-  output: {
-    filename: "js/bundle.[contenthash].min.js",
-    path: resolve(__dirname, "../../dist"),
-    publicPath: "/",
-  },
-  devtool: "source-map",
-  plugins: [],
+	mode: 'production',
+	entry: './index.tsx',
+	output: {
+		filename: 'js/bundle.[contenthash].min.js',
+		path: resolve(__dirname, '../../dist'),
+		publicPath: '/',
+	},
+	devtool: 'source-map',
+	plugins: [
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: resolve(__dirname, '../../src/manifest.json'),
+					to: resolve(__dirname, '../../dist/manifest.json'),
+				},
+			],
+		}),
+	],
 });
