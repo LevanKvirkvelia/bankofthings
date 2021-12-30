e325ae2a20bd47d318e2bba1e4929dadc92b49a8
Levan Kvirkvelia <lekvirkvelia@gmail.com>
2021-12-30 22:16:37 +0300

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,7 @@ const serverUrl = "https://oam0o2ny6nfp.usemoralis.com:2053/server";
 const appId = "t0Nv4m9GahmlrffqRMCCzeWKJdmCzcKrmoUcOpe5";
 
 app.post("/nsupdate", async (req, res) => {
-  const { tokenId } = req.body;
+  const { tokenId } = req.body.object;
   console.log(req.body);
   await Moralis.start({ serverUrl, appId });
   const [tokenNS, token] = await Promise.all([
