d04e1379ca43e802c67232c17ba51bd3ec7c7ab7
Levan Kvirkvelia <lekvirkvelia@gmail.com>
2022-02-02 12:18:41 +0300

    Update useRouterEvents.ts

--- frontend/src/features/analytics/hooks/useRouterEvents.ts
+++ frontend/src/features/analytics/hooks/useRouterEvents.ts
@@ -3,8 +3,9 @@ import { useLocation } from 'react-router-dom';
 import amplitude from 'amplitude-js';
 
 export function useRouterEvents() {
-	const { pathname, search, hash } = useLocation();
+	const location = useLocation();
 	useEffect(() => {
+		const { pathname, search, hash } = location;
 		amplitude.getInstance().logEvent('Navigation_Page_Open', { pathname, search, hash });
 	}, [location]);
 }
