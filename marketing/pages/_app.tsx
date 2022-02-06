9f7b215d256563186557a8f25ab61fb93f0d3776
Levan Kvirkvelia <lekvirkvelia@gmail.com>
2022-02-06 12:25:26 +0300

    use next/head

--- marketing/pages/_app.tsx
+++ marketing/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import { extendTheme } from "@chakra-ui/react";
@@ -14,7 +15,48 @@ const theme = extendTheme({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <>
+        <Head>
+          <title>Bank of Things – Token gated access to Notion</title>
+          <meta
+            name="description"
+            content="Access Notion, Web2 domains, Twitter, and Discord via NFT or DAO token"
+          ></meta>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="https://api.typedream.com/v0/document/public/dd596406-303e-4f2c-b330-3e3e64a5922e_favicon-32x32_png.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            href="https://api.typedream.com/v0/document/public/dd596406-303e-4f2c-b330-3e3e64a5922e_favicon-32x32_png.png"
+          />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:title"
+            content="Bank of Things – Token gated access to Notion"
+          />
+          <meta
+            property="og:description"
+            content="Access Notion, Web2 domains, Twitter, and Discord via NFT or DAO token"
+          />
+          <meta property="og:site_name" content="Bank of Things" />
+          <meta
+            name="twitter:title"
+            content="Bank of Things – Token gated access to Notion"
+          />
+          <meta
+            name="twitter:description"
+            content="Access Notion, Web2 domains, Twitter, and Discord via NFT or DAO token"
+          />
+          <meta property="og:url" content="https://www.bankofthings.com/" />
+        </Head>
+        <Component {...pageProps} />
+      </>
     </ChakraProvider>
   );
 }
